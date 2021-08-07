4f81854cf2bc4c9a92f821816986524841703389
Austin Serb <128577470+Austin1serb@users.noreply.github.com>
2021-08-06 23:51:25 -0700

    Add files via upload

---
+++ Zootest.java
@@ -0,0 +1,26 @@
+public class Zootest {
+    public static void main(String[] args) {
+        // Gorilla Test
+        Gorilla gorilla = new Gorilla();
+        gorilla.throwSomething();
+        gorilla.throwSomething();
+        gorilla.throwSomething();
+        gorilla.eatBananas();
+        gorilla.eatBananas();
+        gorilla.climb();
+        gorilla.displayEnergy();
+
+        System.out.println("\n---------------------\n");
+
+        // Bat Test
+        Bat bat = new Bat();
+        bat.attackTown();
+        bat.attackTown();
+        bat.attackTown();
+        bat.eatHumans();
+        bat.eatHumans();
+        bat.fly();
+        bat.fly();
+        bat.displayEnergy();
+    }
+}
